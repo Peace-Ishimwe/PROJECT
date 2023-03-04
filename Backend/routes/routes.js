f050e1012bbabaa0ac4aabe58195a2a1caff7faa
Peace Ishimwe <peaceishimwem@gmail.com>
2023-03-04 08:33:25 +0200

    start

--- Backend/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import { post , get } from "../controller/controller.js";
-import  express  from "express";
-const route  = express.Router()
-
-route.post('/signup' , post)
-route.get('/' , get)
-
-export default route
